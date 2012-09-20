930d819f770ce4d76697268141c05aabbc7e8b90
Krish Verma <kverma@gmail.com>
2012-09-21 01:48:06 +0800

    1. Added hammer.js for swipe/tap event handling 2. Added icon for app

--- src/app/GameScreen.js
+++ src/app/GameScreen.js
@@ -1,25 +1,25 @@
 // Game Screen View
 
 GameScreen = Backbone.View.extend({
-    el: $('#main-body'),
+	el: $('#main-body'),
     template:_.template($("#game-screen-tpl").html()),
     playing: false,
     moveCounter: 0,
 
 
     events:{
-	"click #B1": "back",
-	"click #B2": "shuffle",
-	"click #B3": "hint",
+		"click #B1": "back",
+		"click #B2": "shuffle",
+		"click #B3": "hint",
         "click #B4": "toggleplay",
         "click #B5": "changepic"
-   },
+	},
 
     initialize:function () {
         console.log("initing game screen....");
 
-	ctlr = new SliderCtlr(parseInt(this.options.numofrows)+2, 
-			      this.options.boardsize);
+		ctlr = new SliderCtlr(parseInt(this.options.numofrows)+2, 
+						      this.options.boardsize);
 
 //	this.playing = false;
 //	this.moveCounter = -1;
@@ -28,76 +28,76 @@ GameScreen = Backbone.View.extend({
     },
 
     render:function () {
-	var html = this.template();
-	$(this.el).html(html);
+		var html = this.template();
+		$(this.el).html(html);
     },
     
    
     toggleplay: function (e) {
-	e.preventDefault();
-	console.log("Play button toggleed...");
-	var hiddenInput;
-	if(!this.playing)
-	{
-	    this.$('#B2').attr('disabled', true);
+		e.preventDefault();
+		console.log("Play button toggleed...");
+		var hiddenInput;
+		if(!this.playing)
+		{
+		    this.$('#B2').attr('disabled', true);
 //	    hiddenInput.off('click', 'shuffle');
-	    hiddenInput = this.$('#start');	   
-	    ctlr.addListeners();
-	}
-	else
-	{
+		    hiddenInput = this.$('#start');	   
+		    ctlr.addListeners();
+		}
+		else
+		{
 //	    hiddenInput = this.$('#B2');	   
-	    this.$('#B2').attr('disabled', false);
+		    this.$('#B2').attr('disabled', false);
 //	    hiddenInput.on('click', 'shuffle');
-	    hiddenInput = this.$('#stop');	   
-	    ctlr.removeListeners();
-	}
-	hiddenInput.click();
-	this.playing = !this.playing;
+		    hiddenInput = this.$('#stop');	   
+		    ctlr.removeListeners();
+		}
+		hiddenInput.click();
+		this.playing = !this.playing;
     },
 
     shuffle: function (e) {
-	e.preventDefault();
-	console.log("shuffle button pressd...");
-	if(!this.playing)
-	{
-	    var hiddenInput = this.$('#reset');
-	    hiddenInput.click();
-	    this.moveCounter = -1;
-	    this.playing = false;	    
-	    ctlr.shuffleTiles();
-	    this.incrMoves(e);
-	}
-	else
-	{
-	    console.log("do nothing as we are playing");
-	}
+		e.preventDefault();
+		console.log("shuffle button pressd...");
+		if(!this.playing)
+		{
+		    var hiddenInput = this.$('#reset');
+		    hiddenInput.click();
+		    this.moveCounter = -1;
+		    this.playing = false;	    
+		    ctlr.shuffleTiles();
+		    this.incrMoves(e);
+		}
+		else
+		{
+		    console.log("do nothing as we are playing");
+		}
     },
 
     hint: function (e) {
-	e.preventDefault();
-	console.log("hint button pressd...");
-	ctlr.toggleHintDisplay();
+		e.preventDefault();
+		console.log("hint button pressd...");
+		ctlr.toggleHintDisplay();
     },
 
     changepic: function (e) {
-	e.preventDefault();
-	ctlr.changePic();
-	this.shuffle(e);
+		e.preventDefault();
+		ctlr.changePic();
+		this.shuffle(e);
     },
 
     incrMoves: function () {
 //	e.preventDefault();
-	var ctr = document.getElementById('movecount');
-	ctr.innerText = "Moves "+(++this.moveCounter);
-	console.log("incrementing button pressd...");
+		var ctr = document.getElementById('movecount');
+		ctr.innerText = "Moves "+(++this.moveCounter);
+		console.log("incrementing button pressd...");
     },
     
     back:function (e) {
-	e.preventDefault();
+		e.preventDefault();
         console.log("Exiting game and going back to start page");
         //unbind event listeners so that obj gets cleanedup
-	ctlr.removeListeners();
+		ctlr.removeListeners();
         this.$el.off();
         App.navigate("#", {trigger:true});
     }
--- src/app/Slider.js
+++ src/app/Slider.js
@@ -7,129 +7,164 @@ function SliderCtlr (numberofrows, boardsize) {
 
 SliderCtlr.prototype = {
     init: function() {
-	ctlr.img = new Image();
-	ctlr.tileSize = ctlr.boardSize / ctlr.tileCount;
-	ctlr.clickLoc = new Object;
-	ctlr.clickLoc.x = 0;
-	ctlr.clickLoc.y = 0;
-	ctlr.emptyLoc = new Object;
-	ctlr.emptyLoc.x = 0;
-	ctlr.emptyLoc.y = 0;
-	ctlr.solved = false;
-	ctlr.boardParts = new Object;
-	ctlr.canvas = null
-	ctlr.context = null;
-	ctlr.showhint = false;
-	ctlr.setBoard();
-	ctlr.getCanvasCtx();
+		ctlr.img = new Image();
+		ctlr.tileSize = ctlr.boardSize / ctlr.tileCount;
+		ctlr.clickLoc = new Object;
+		ctlr.clickLoc.x = 0;
+		ctlr.clickLoc.y = 0;
+		ctlr.emptyLoc = new Object;
+		ctlr.emptyLoc.x = 0;
+		ctlr.emptyLoc.y = 0;
+		ctlr.solved = false;
+		ctlr.boardParts = new Object;
+		ctlr.canvas = null
+		ctlr.context = null;
+		ctlr.offsetLeft = 0;
+		ctlr.offsetTop = 0;
+		ctlr.hammer = null;
+		ctlr.showhint = false;
+		ctlr.setBoard();
+		ctlr.getCanvasCtx();
     },
 
     getCanvasCtx:function() {
-	ctlr.canvas = document.getElementById('puzzle');
-	ctlr.context = ctlr.canvas.getContext('2d');
-	ctlr.img.src = 'http://www.brucealderman.info/Images/dimetrodon.jpg';
-	ctlr.img.addEventListener('load', this.drawTiles, false);
+		ctlr.canvas = document.getElementById('puzzle');
+		ctlr.offsetLeft = ctlr.canvas.offsetLeft;
+		ctlr.offsetTop = ctlr.canvas.offsetTop;		
+		ctlr.context = ctlr.canvas.getContext('2d');
+		ctlr.img.src = 'resources/img/picsaw-splash.png';
+		ctlr.img.addEventListener('load', this.drawTiles, false);
 	
 //	ctlr.context.font(
     },
 
     setBoard:function() {
-	ctlr.boardParts = new Array(ctlr.tileCount);
-	for (var i = 0; i < ctlr.tileCount; ++i) {
-	    ctlr.boardParts[i] = new Array(ctlr.tileCount);
-	    for (var j = 0; j < ctlr.tileCount; ++j) {
-		ctlr.boardParts[i][j] = new Object;
-		ctlr.boardParts[i][j].x = (ctlr.tileCount - 1) - i;
-		ctlr.boardParts[i][j].y = (ctlr.tileCount - 1) - j;
-//		ctlr.boardParts[i][j].x = i;
-//		ctlr.boardParts[i][j].y = j;
-	    }
-	}
-	ctlr.emptyLoc.x = ctlr.boardParts[ctlr.tileCount - 1][ctlr.tileCount - 1].x;
-	ctlr.emptyLoc.y = ctlr.boardParts[ctlr.tileCount - 1][ctlr.tileCount - 1].y;
-//	ctlr.emptyLoc.x = -1;
-//	ctlr.emptyLoc.y = -1;
-	ctlr.solved = false;
+		ctlr.boardParts = new Array(ctlr.tileCount);
+		for (var i = 0; i < ctlr.tileCount; ++i) {
+		    ctlr.boardParts[i] = new Array(ctlr.tileCount);
+		    for (var j = 0; j < ctlr.tileCount; ++j) {
+			ctlr.boardParts[i][j] = new Object;
+			ctlr.boardParts[i][j].x = (ctlr.tileCount - 1) - i;
+			ctlr.boardParts[i][j].y = (ctlr.tileCount - 1) - j;
+	//		ctlr.boardParts[i][j].x = i;
+	//		ctlr.boardParts[i][j].y = j;
+		    }
+		}
+		ctlr.emptyLoc.x = ctlr.boardParts[ctlr.tileCount - 1][ctlr.tileCount - 1].x;
+		ctlr.emptyLoc.y = ctlr.boardParts[ctlr.tileCount - 1][ctlr.tileCount - 1].y;
+	//	ctlr.emptyLoc.x = -1;
+	//	ctlr.emptyLoc.y = -1;
+		ctlr.solved = false;
     },
 
     drawTiles:function() {
-	ctlr.context.clearRect ( 0 , 0 , ctlr.boardSize , ctlr.boardSize );
-	for (var i = 0; i < ctlr.tileCount; ++i) {
-	    for (var j = 0; j < ctlr.tileCount; ++j) {
-		var x = ctlr.boardParts[i][j].x;
-		var y = ctlr.boardParts[i][j].y;
-		if(i != ctlr.emptyLoc.x || 
-		   j != ctlr.emptyLoc.y || 
-		   ctlr.solved == true) {		   
-		    ctlr.context.drawImage(ctlr.img, 
-					   x * ctlr.tileSize, 
-					   y * ctlr.tileSize, 
-					   ctlr.tileSize, 
-					   ctlr.tileSize,
-					   i * ctlr.tileSize, 
-					   j * ctlr.tileSize, 
-					   ctlr.tileSize, 
-					   ctlr.tileSize);
-		    ctlr.context.strokeStyle="white";
-		    ctlr.context.strokeRect(i * ctlr.tileSize, 
-					    j * ctlr.tileSize, 
-					    ctlr.tileSize,
-					    ctlr.tileSize);
-		    if(ctlr.showhint) {
-			ctlr.context.font="12pt Ubuntu";
-			ctlr.context.strokeStyle="black";
-			ctlr.context.fillStyle="yellow";
-			ctlr.context.strokeText(" "+(ctlr.tileCount*y+x+1), 
-					    i * ctlr.tileSize+10, 
-					    j * ctlr.tileSize+20);
-			ctlr.context.fillText(" "+(ctlr.tileCount*y+x+1), 
-					  i * ctlr.tileSize+10, 
-					  j * ctlr.tileSize+20);
+		ctlr.context.clearRect ( 0 , 0 , ctlr.boardSize , ctlr.boardSize );
+		for (var i = 0; i < ctlr.tileCount; ++i) {
+		    for (var j = 0; j < ctlr.tileCount; ++j) {
+				var x = ctlr.boardParts[i][j].x;
+				var y = ctlr.boardParts[i][j].y;
+				if(i != ctlr.emptyLoc.x || 
+				   j != ctlr.emptyLoc.y || 
+				   ctlr.solved == true) {		   
+				    ctlr.context.drawImage(ctlr.img, 
+							   x * ctlr.tileSize, 
+							   y * ctlr.tileSize, 
+							   ctlr.tileSize, 
+							   ctlr.tileSize,
+							   i * ctlr.tileSize, 
+							   j * ctlr.tileSize, 
+							   ctlr.tileSize, 
+							   ctlr.tileSize);
+				    ctlr.context.strokeStyle="white";
+				    ctlr.context.strokeRect(i * ctlr.tileSize, 
+							    j * ctlr.tileSize, 
+							    ctlr.tileSize,
+							    ctlr.tileSize);
+				    if(ctlr.showhint) {
+						ctlr.context.font="12pt Ubuntu";
+						ctlr.context.strokeStyle="black";
+						ctlr.context.fillStyle="yellow";
+						ctlr.context.strokeText(" "+(ctlr.tileCount*y+x+1), 
+								    i * ctlr.tileSize+10, 
+								    j * ctlr.tileSize+20);
+						ctlr.context.fillText(" "+(ctlr.tileCount*y+x+1), 
+								  i * ctlr.tileSize+10, 
+								  j * ctlr.tileSize+20);
+				    }
+				}
 		    }
-
 		}
-	    }
-	}
     },
 
-    
+    swipeddistance:function(direction, x1, y1, x2, y2) {
+    	console.log("SW "+direction+" : clickLoc: ("+x1+","+y1+") : emptyLoc: ("+x2+","+y2+")");
+    	switch(direction) {
+    		case "left":
+    			if(y1 == y2) 
+    			{
+ 					return	(x1 - x2);
+    			}
+    			break;
+    		case "right":
+	    		if(y1 == y2) 
+    			{
+ 					return	(x2 - x1);
+    			}
+    			break;
+    		case "up": 
+    			if(x1 == x2) 
+    			{
+ 					return	(y1 - y2);
+    			}
+    			break;
+    		case "down":
+	    		if(x1 == x2) 
+    			{
+ 					return	(y2 - y1);
+    			}
+    			break;
+    	}
+    	return 0;
+    },
+	    
     distance:function(x1, y1, x2, y2) {
-	return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    	console.log("Tap : clickLoc: ("+x1+","+y1+") : emptyLoc: ("+x2+","+y2+")");
+		return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     },
 
     slideTile:function(toLoc, fromLoc) {
-	if (!ctlr.solved) {
-	    ctlr.boardParts[toLoc.x][toLoc.y].x = 
-		ctlr.boardParts[fromLoc.x][fromLoc.y].x;
-	    ctlr.boardParts[toLoc.x][toLoc.y].y = 
-		ctlr.boardParts[fromLoc.x][fromLoc.y].y;
-	    ctlr.boardParts[fromLoc.x][fromLoc.y].x = 
-		ctlr.tileCount - 1;
-	    ctlr.boardParts[fromLoc.x][fromLoc.y].y = 
-		ctlr.tileCount - 1;
-	    toLoc.x = fromLoc.x;
-	    toLoc.y = fromLoc.y;
-	    gs.incrMoves();
-	    ctlr.checkSolved();
-	}
+		if (!ctlr.solved) {
+		    ctlr.boardParts[toLoc.x][toLoc.y].x = 
+			ctlr.boardParts[fromLoc.x][fromLoc.y].x;
+		    ctlr.boardParts[toLoc.x][toLoc.y].y = 
+			ctlr.boardParts[fromLoc.x][fromLoc.y].y;
+		    ctlr.boardParts[fromLoc.x][fromLoc.y].x = 
+			ctlr.tileCount - 1;
+		    ctlr.boardParts[fromLoc.x][fromLoc.y].y = 
+			ctlr.tileCount - 1;
+		    toLoc.x = fromLoc.x;
+		    toLoc.y = fromLoc.y;
+		    gs.incrMoves();
+		    ctlr.checkSolved();
+		}
     },
     
     checkSolved: function() {
-	var flag = true;
-	for (var i = 0; i < ctlr.tileCount; ++i) {
-	    for (var j = 0; j < ctlr.tileCount; ++j) {
-		if (ctlr.boardParts[i][j].x != i || 
-		    ctlr.boardParts[i][j].y != j) {
-		    flag = false;
+		var flag = true;
+		for (var i = 0; i < ctlr.tileCount; ++i) {
+		    for (var j = 0; j < ctlr.tileCount; ++j) {
+				if (ctlr.boardParts[i][j].x != i || 
+				    ctlr.boardParts[i][j].y != j) {
+				    flag = false;
+				}
+			}
 		}
-	    }
-	}
-	ctlr.solved = flag;
+		ctlr.solved = flag;
     },
 
     toggleHintDisplay: function () {
-	ctlr.showhint = !ctlr.showhint;
-	ctlr.drawTiles();
+		ctlr.showhint = !ctlr.showhint;
+		ctlr.drawTiles();
     },
 
     shuffleTiles: function() {
@@ -137,22 +172,63 @@ SliderCtlr.prototype = {
     },
 
     addListeners: function() {
-	ctlr.canvas.onclick = function(e) {
-	    ctlr.clickLoc.x = Math.floor((e.pageX - this.offsetLeft) / 
-					 ctlr.tileSize);
-	    ctlr.clickLoc.y = Math.floor((e.pageY - this.offsetTop) / 
-					 ctlr.tileSize);   
-	    if (ctlr.distance(ctlr.clickLoc.x, 
-			      ctlr.clickLoc.y, 
-			      ctlr.emptyLoc.x, 
-			      ctlr.emptyLoc.y) == 1) {
-		ctlr.slideTile(ctlr.emptyLoc, ctlr.clickLoc);
-		ctlr.drawTiles();
-	    }
-	    if (ctlr.solved) {
-		setTimeout(function() {alert("You solved it!");}, 500);
-	    }
-	};
+    	hammer = new Hammer(ctlr.canvas); 
+    	hammer.onswipe = function(ev) {
+    		var xx = ev.position.x - ev.distanceX;
+			var yy = ev.position.y - ev.	distanceY;
+			
+    		console.log(ev.type+ " : "+ev.direction+" X:X "+ev.position.x+" : "+xx+" Y:Y "+ev.position.y+" : "+yy);
+    		ctlr.clickLoc.x = Math.floor( xx / ctlr.tileSize);
+		    ctlr.clickLoc.y = Math.floor( yy / ctlr.tileSize);   
+		    if (ctlr.swipeddistance(ev.direction, ctlr.clickLoc.x, 
+				      ctlr.clickLoc.y, 
+				      ctlr.emptyLoc.x, 
+				      ctlr.emptyLoc.y) == 1) {
+				ctlr.slideTile(ctlr.emptyLoc, ctlr.clickLoc);
+				ctlr.drawTiles();
+		    }
+		    if (ctlr.solved) {
+				setTimeout(function() {alert("You solved it!");}, 500);
+		    }
+    	}; 
+
+		hammer.ontap = function(ev) {
+			var xx = ev.position[0].x - ctlr.offsetLeft;
+			var yy = ev.position[0].y - ctlr.offsetTop;
+			
+    		console.log(ev.type+ " X:X "+ev.position[0].x+" : "+xx+" Y:Y "+ev.position[0].y+" : "+yy);
+			ctlr.clickLoc.x = Math.floor( xx / ctlr.tileSize);
+		    ctlr.clickLoc.y = Math.floor( yy / ctlr.tileSize);   
+		    if (ctlr.distance(ctlr.clickLoc.x, 
+				      ctlr.clickLoc.y, 
+				      ctlr.emptyLoc.x, 
+				      ctlr.emptyLoc.y) == 1) {
+				ctlr.slideTile(ctlr.emptyLoc, ctlr.clickLoc);
+				ctlr.drawTiles();
+		    }
+		    if (ctlr.solved) {
+				setTimeout(function() {alert("You solved it!");}, 500);
+		    }
+    	}; 
+
+
+		ctlr.canvas.onclick1 = function(e) {
+			var xx = e.pageX - ctlr.offsetLeft;
+			var yy = e.pageY - ctlr.offsetTop;
+			console.log("X:X "+e.pageX+" : "+xx+" Y:Y "+e.pageY+" : "+yy);
+		    ctlr.clickLoc.x = Math.floor( xx / ctlr.tileSize);
+		    ctlr.clickLoc.y = Math.floor( yy / ctlr.tileSize);   
+		    if (ctlr.distance(ctlr.clickLoc.x, 
+				      ctlr.clickLoc.y, 
+				      ctlr.emptyLoc.x, 
+				      ctlr.emptyLoc.y) == 1) {
+				ctlr.slideTile(ctlr.emptyLoc, ctlr.clickLoc);
+				ctlr.drawTiles();
+		    }
+		    if (ctlr.solved) {
+				setTimeout(function() {alert("You solved it!");}, 500);
+		    }
+		};
     },
 
     removeListeners: function() {
